ebda0351442534b009ee0cfbb0af28040311591b
gaelforget <gforget@mit.edu>
2022-04-01 11:57:29 -0400

    download list_files as part of docs build

--- src/GDAC.jl
+++ src/GDAC.jl
@@ -53,6 +53,7 @@ Get list of Argo float files from csv file with columns two columns -- `folder` 
 
 ```
 using ArgoData
+fil="https://raw.githubusercontent.com/JuliaOcean/ArgoData.jl/gh-pages/dev/Argo_float_files.csv"
 files_list=GDAC.Argo_files_list(fil)
 ```
 """

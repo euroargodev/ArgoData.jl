53022e8f2f569d18bdbdb5d56a67f37b42a68aab
gaelforget <gforget@mit.edu>
2022-04-11 20:32:35 -0400

    reorganize as MITprof_plots

--- examples/MITprof_plots.jl
+++ examples/MITprof_plots.jl
@@ -1,13 +1,59 @@
 
-module MITprof_stats
+module MITprof_plots
 
-using ArgoData, DataFrames, CSV, Dates, GLMakie
+using ArgoData, DataFrames, CSV, Dates, GLMakie, JLD2, Statistics
+
+#trim_cost is used in plot_cost
+function trim_cost(cost)
+    ii=findall((!ismissing).(cost) .&& (isfinite).(cost) .&& (cost.>0.0))
+    med=round(median(cost[ii]), digits=3)
+    nb=length(ii)
+
+    (ncost,nii)=(length(cost),length(ii))
+    println("keeping $(ncost), leaving $(ncost-nii)")
+    println("median $(median(cost[ii]))")
+    
+    cost[ii],med,nb
+end
+
+"""
+    plot_cost()
+
+```
+f=MITprof_plots.plot_cost()
+save("cost_pdf.png", f)
+```
+"""
+function plot_cost()
+    #
+    costT=load("prof_T_stats.jld2","cost")
+    costT,medT,nbT=trim_cost(costT)
+    #
+    costS=load("prof_S_stats.jld2","cost")
+    costS,medS,nbS=trim_cost(costS)
+
+    f = Figure()
+    hist(f[1, 1], costT, bins = collect(0:0.1:8.0), 
+        axis = (title = "cost for T (median=$(medT), from $(nbT))",), 
+        color = :values, strokewidth = 1, strokecolor = :black)
+
+    hist(f[2, 1], costS, bins = collect(0:0.1:8.0), 
+        axis = (title = "cost for S (median=$(medS), from $(nbS))",),
+        color = :values, strokewidth = 1, strokecolor = :black)
+
+    f
+end
 
 """
     plot_stats()
 
 Read file `profile_positions.csv`, pre-process, and then compute 
 and display basic statistics of the Argo float array.
+
+```
+f=MITprof_plots.plot_stats()
+save("ArgoDistributions.png", f)
+```
 """
 function plot_stats()
     csv_file="profile_positions.csv"
@@ -72,4 +118,5 @@ function plot_stats(df::DataFrame; ym1=2004+(12-0.5)/12,ym2=2021+(12-0.5)/12)
 	f
 end
 
-end #module MITprof_stats
+
+end

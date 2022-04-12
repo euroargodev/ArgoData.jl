a037aa307e343f1b62a971b8d5ed70e1c88aadbb
gaelforget <gforget@mit.edu>
2022-04-11 20:25:12 -0400

    mv cost_functions and profile_positions to MITprof module + test

--- examples/MITprof_inspect.jl
+++ examples/MITprof_inspect.jl
@@ -1,59 +1,9 @@
 
 module MITprof_inspect
 
-using Glob, NCDatasets, JLD2, Statistics
+using JLD2, Statistics
 
 import GLMakie as Mkie
-
-"""
-    check_stats_cost(vv="prof_T",JJ=[])
-
-Loop through files and compute nb profiles, nb non-blank profiles, nb levels mean, cost mean.
-
-```
-nt,np,nz,cost=check_stats_cost("prof_S")
-jldsave("prof_S_stats.jld2"; nt,np,nz,cost)
-```
-"""
-function check_stats_cost(vv="prof_T",JJ=[])
-    pth=joinpath(tempdir(),"Argo_MITprof_files","MITprof")
-    list_nc=glob("*.nc",pth)
-    list_txt=glob("*.txt",pth)
-
-    nt=[]
-    np=[]
-    nz=[]
-    cost=[]
-
-    isempty(JJ) ? II=(1:length(list_nc)) : II=JJ
-    time0=time()
-    for ii in II
-        mod(ii,100)==0 ? println(time()-time0) : nothing
-        mod(ii,100)==0 ? println(ii) : nothing
-        ds=Dataset(list_nc[ii])
-        push!(nt,size(ds[vv],1))
-        tmp1=sum((!ismissing).(ds[vv]),dims=2)
-        tmp2=sum(tmp1.>0)
-        if tmp2>0
-            push!(np,tmp2)
-            push!(nz,sum(tmp1)/tmp2)
-            tmp1=(ds[vv]-ds[vv*"estim"]).^2 .*ds[vv*"weight"]
-            if ~isa(tmp1,Matrix{Missing})
-                tmp1[findall(ismissing.(tmp1))].=0.0
-                push!(cost,sum(tmp1[:])/sum((tmp1[:].>0.0)))
-            else
-                push!(cost,missing)
-            end
-        else
-            push!(np,0)
-            push!(nz,0)
-            push!(cost,0)
-        end
-        close(ds)
-    end
-
-    return nt,np,nz,cost
-end
 
 #trim_cost is used in plot_cost
 function trim_cost(cost)

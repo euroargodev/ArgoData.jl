47c70393e19c112e97066349a7e932a2123addfe
gaelforget <gforget@mit.edu>
2023-01-07 21:33:06 -0100

    add mising AnalysisMethods module

--- src/ArgoData.jl
+++ src/ArgoData.jl
@@ -11,7 +11,7 @@ include("MITprof.jl")
 include("MITprofAnalysis.jl")
 
 export GDAC, ArgoTools, GriddedFields
-export MITprof, MITprofAnalysis, MITprofStat
+export MITprof, MITprofAnalysis, MITprofStat, AnalysisMethods
 export ProfileNative, ProfileStandard, MITprofStandard
 
 end # module

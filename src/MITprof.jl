8bf88e878933bc33e186abc0c4b54633f9a2107f
gaelforget <gforget@mit.edu>
2023-01-07 21:32:30 -0100

    rm duplicate code cost_functions

--- src/MITprof.jl
+++ src/MITprof.jl
@@ -391,61 +391,6 @@ using Dates, MeshArrays, NCDatasets, Glob, DataFrames, CSV
 import ArgoData.MITprofStandard
 
 """
-    cost_functions(vv="prof_T",JJ=[])
-
-Loop through files and compute nb profiles, nb non-blank profiles, nb levels mean, cost mean.
-
-```
-pth="MITprof/"
-nt,np,nz,cost=MITprof.cost_functions(pth,"prof_S")
-
-using JLD2
-jldsave(joinpath("csv","prof_S_stats.jld2"); nt,np,nz,cost)
-```
-"""
-function cost_functions(pth,vv="prof_T",JJ=[])
-    list_nc=glob("*.nc",pth)
-    list_txt=glob("*.txt",pth)
-
-    nt=[]
-    np=[]
-    nz=[]
-    cost=[]
-
-    isempty(JJ) ? II=(1:length(list_nc)) : II=JJ
-    isa(II,String) ? II=findall(basename.(list_nc).==JJ)[1] : nothing 
-
-    time0=time()
-    for ii in II
-        mod(ii,100)==0 ? println(time()-time0) : nothing
-        mod(ii,100)==0 ? println(ii) : nothing
-        ds=Dataset(list_nc[ii])
-        push!(nt,size(ds[vv],1))
-        tmp1=sum((!ismissing).(ds[vv]),dims=2)
-        tmp2=sum(tmp1.>0)
-        if tmp2>0
-            push!(np,tmp2)
-            push!(nz,sum(tmp1)/tmp2)
-            tmp1=(ds[vv]-ds[vv*"estim"]).^2 .*ds[vv*"weight"]
-            if ~isa(tmp1,Matrix{Missing})
-                tmp1[findall(ismissing.(tmp1))].=0.0
-                push!(cost,sum(tmp1[:])/sum((tmp1[:].>0.0)))
-            else
-                push!(cost,missing)
-            end
-        else
-            push!(np,0)
-            push!(nz,0)
-            push!(cost,0)
-        end
-        close(ds)
-    end
-
-    return nt,np,nz,cost
-end
-
-
-"""
     profile_positions(path)
 
 Create table (`DataFrame`) of the positions and dates obtained by looping through files in `path`. 

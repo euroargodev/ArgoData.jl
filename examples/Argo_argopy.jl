dbdc70750f736936d7c736de527f9373950d7324
gaelforget <gforget@mit.edu>
2024-10-07 03:12:43 -0400

    bump version

--- examples/Argo_argopy.jl
+++ examples/Argo_argopy.jl
@@ -130,7 +130,7 @@ Suppressor = "~0.2.8"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.10.3"
+julia_version = "1.10.5"
 manifest_format = "2.0"
 project_hash = "254a4131f1040b247742aae2901b7e9d0230d5df"
 
@@ -2172,7 +2172,7 @@ version = "0.15.2+0"
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.8.0+1"
+version = "5.11.0+0"
 
 [[deps.libfdk_aac_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl"]

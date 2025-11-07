062d6eb3805e6fd43cd8ec0e51e1ec9f065e8e30
Enrico <enrico.milanese@whoi.edu>
2025-11-07 10:14:02 -0500

    remove old definition of get_subset_region

--- src/Parquet.jl
+++ src/Parquet.jl
@@ -63,26 +63,6 @@ end
     dates=DateTime("2001-01-01T00:00:00") .. DateTime("2024-12-31T23:59:59"),
     variables=(:JULD, :LATITUDE, :LONGITUDE, :PRES, :TEMP, :PLATFORM_NUMBER))
 """
-function get_subset_region(ds2::Parquet2.Dataset; lons=-75 .. -50, lats=25 .. 40, 
-    dates=DateTime("2001-01-01T00:00:00") .. DateTime("2024-12-31T23:59:59"),
-    variables=(:JULD, :LATITUDE, :LONGITUDE, :PRES, :TEMP, :PLATFORM_NUMBER))
-
-    rule_juld = x -> in.(coalesce.(x, DateTime("2100-01-01T00:00:00")), Ref(dates))
-    rule_latitude = x -> in.(coalesce.(x, NaN), Ref(lats))
-    rule_longitude = x -> in.(coalesce.(x, NaN), Ref(lons))
-    rule_row_group = x ->   any(rule_latitude(Parquet2.load(x["LATITUDE"]))) && 
-                            any(rule_longitude(Parquet2.load(x["LONGITUDE"]))) && 
-                            any(rule_juld(Parquet2.load(x["JULD"])))
-
-    row_groups = filter(rule_row_group, ds2.row_groups)
-    df_subset2 = reduce(vcat,DataFrame.(row_groups,copycols=false)) |> TO.select(variables...) |> DataFrame
-    subset!(df_subset2, :LATITUDE => rule_latitude)
-    subset!(df_subset2, :LONGITUDE => rule_longitude)
-    subset!(df_subset2, :JULD => rule_juld)
-    df_subset2
-end
-
-"""
     get_subset_float(ds2::Parquet2.Dataset ; ID=3901064,
     variables=(:JULD, :LATITUDE, :LONGITUDE, :PRES, :TEMP, :PLATFORM_NUMBER))
     rule_PLATFORM_NUMBER = x -> coalesce.(x, 0).==Ref(string(ID))
